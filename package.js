f3af8b945a94e92a7b92fc4d062b2af215067d10
Jaap van der Meer <jaapvandermeer@gmail.com>
2015-04-17 12:45:05 +0200

    Added github url.

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'japetheape:class-helpers',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Tiny package to add class functionality to Meteor in general and to collections.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/japetheape/class-helpers',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
